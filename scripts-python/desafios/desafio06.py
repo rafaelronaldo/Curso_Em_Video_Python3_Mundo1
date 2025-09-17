8920892ae72ec3e91de3208f72ecba4506d02060
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-17 16:58:10 -0300

    Desafio 06

---
+++ scripts-python/desafios/desafio06.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um número: "))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (1/2)
+
+print("O número que você digitou foi: {}".format(numero))
+print("O dobro deste número é: {}".format(dobro))
+print("O triplo deste número é: {}".format(triplo))
+print("A raiz quadrada deste número é: {}".format(raiz_quadrada))
