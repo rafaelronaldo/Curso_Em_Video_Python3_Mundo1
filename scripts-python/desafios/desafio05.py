bc4cfbdf1437c411a3ab8a0f81acbd6231759f97
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-17 16:50:51 -0300

    Desafio 05

---
+++ scripts-python/desafios/desafio05.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+sucessor = numero + 1
+antecessor = numero - 1
+
+print("O número que você digitou foi: {}".format(numero))
+print("O sucessor deste número é: {}".format(sucessor))
+print("O antecessor deste número é: {}".format(antecessor))
