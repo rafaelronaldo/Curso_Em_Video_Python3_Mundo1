42f8dd4a9ddaf2d4a2d721309973c4e26bdd66ed
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-17 16:47:43 -0300

    Aula 7 - Testes

---
+++ scripts-python/testes-aula/aula07a copy.py
@@ -0,0 +1,17 @@
+numero1 = int(input("Um valor: "))
+numero2 = int(input("Outro valor: "))
+soma = numero1 + numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+potencia = numero1 ** numero2
+divisao_inteira = numero1 // numero2
+resto_divisao = numero1 % numero2
+
+print("A soma é: {}".format(soma))
+print("A multiplicação é: {}".format(multiplicacao))
+print("A divisão é: {:.3f}".format(divisao))
+print("A potência é: {}".format(potencia))
+print("A divisão inteira é: {}".format(divisao_inteira))
+print("O resto da divisão é: {}". format(resto_divisao))
+
+
