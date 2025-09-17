1999b758f161ac238d31e8b73d5bb753c6b422e4
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-16 23:14:00 -0300

    Aula 06 - Teste

---
+++ scripts-python/testes-aula/aula06a.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+
+print("A soma do numero {} e {} é igual a: {}".format (numero1, numero2, soma))
