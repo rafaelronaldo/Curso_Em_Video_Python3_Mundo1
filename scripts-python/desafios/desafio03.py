62fba3cc44e5c6585fd025d0c4e9b03db6a39427
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-16 23:16:24 -0300

    Update desafio03

--- scripts-python/desafios/desafio03.py
+++ scripts-python/desafios/desafio03.py
@@ -2,4 +2,4 @@ print ("====== Desafio 03 ======")
 num1 = int(input ("Primeiro número: "))
 num2 = int(input ("Segundo número: "))
 soma = num1 + num2
-print ("A soma é: ", soma)
+print ("A soma é: {}". format (soma))
