0e8d167e2bc57123494c76addd357b240ad835c0
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-18 16:11:04 -0300

    Desafio 13

---
+++ scripts-python/desafios/desafio13.py
@@ -0,0 +1,6 @@
+salario = float(input("Digite seu salário: "))
+aumento = salario * 15/100
+novo_salario = salario + aumento
+
+print("Salário sem aumento: {}".format(salario))
+print("Salário com aumento: {}".format(novo_salario))
