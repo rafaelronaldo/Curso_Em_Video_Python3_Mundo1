1155e1ec1e4c5ea8ac95322856ed05a60d269d5b
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-18 15:37:11 -0300

    Desafio 08

---
+++ scripts-python/desafios/desafio08.py
@@ -0,0 +1,7 @@
+metros = float(input("Digite um valor em metros: "))
+centimetros = metros * 100
+milimetros = metros * 1000
+
+print("Valor em metros: {:0.2f} m".format(metros))
+print("Valor em centimetros: {:0.2f} cm".format(centimetros))
+print("Valor em milimetros: {:0.2f} mm".format(milimetros))
