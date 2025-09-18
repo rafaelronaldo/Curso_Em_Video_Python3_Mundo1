5037c7eb7200edece8541addd6c2b4d80d57fb97
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-18 16:00:13 -0300

    Desafio 11

---
+++ scripts-python/desafios/desafio11.py
@@ -0,0 +1,7 @@
+largura = float(input("Largura: "))
+altura = float(input("Altura: "))
+area = altura * largura
+tinta = area / 2
+
+print("Sua parede tem a largura de {} e altura de {}.".format(largura, altura))
+print("Para pintar você precisará de {} litros de tinta".format(tinta))
