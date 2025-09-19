b6958dd523de1a6cd5cfc647306df1d04958a0b7
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-19 15:57:06 -0300

    Exercicio 06

---
+++ scripts-python/exercicios/ex006.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (1/2)
+
+print("O dobro de {} vale {}".format(numero, dobro))
+print("O triplo de {} vale {}".format(numero, triplo))
+print("A raiz quadrada de {} vale {}".format(numero, raiz_quadrada))
