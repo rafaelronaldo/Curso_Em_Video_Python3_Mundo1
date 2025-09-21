46ca719740f575e9c95068cbe36ecc0560a53dde
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-21 11:18:54 -0300

    Aula 08

---
+++ scripts-python/testes-aula/aula08a copy.py
@@ -0,0 +1,4 @@
+from math import sqrt #importa apenas a bilioteca sqrt (raiz quadrada)
+numero = int(input("Digite um número: "))
+raiz_quadrada = sqrt(numero)
+print("A raiz de {} é igual a {:0.2f}".format(numero, raiz_quadrada))
---
+++ scripts-python/testes-aula/aula08a.py
@@ -0,0 +1,4 @@
+import math #importou a biblioteca de matemática
+numero = int(input("Digite um número: "))
+raiz_quadrada = math.sqrt(numero)
+print("A raiz de {} é igual a {}".format(numero, raiz_quadrada)
