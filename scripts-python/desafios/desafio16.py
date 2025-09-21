c99af75a0223ea00d114ac9e2872090252ce0bd1
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-21 11:21:25 -0300

    Desafio 16

---
+++ scripts-python/desafios/desafio16.py
@@ -0,0 +1,6 @@
+import math
+numero = float(input("Digite um número: "))
+parte_inteira = math.trunc(numero)
+
+print("Número digitado: {}".format(numero))
+print("Parte inteira: {}".format(parte_inteira))
