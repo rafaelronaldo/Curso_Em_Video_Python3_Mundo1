00c402194d637df69ce27956c58275626a818799
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-27 13:55:54 -0300

    Exercicio 27

---
+++ scripts-python/exercicios/ex027.py
@@ -0,0 +1,4 @@
+nome_completo = str(input("Digite o seu nome completo: "))
+nome_completo = nome_completo.split()
+print("Seu primeiro nome é {}".format(nome_completo[0]))
+print("Seu último nome é {}".format(nome_completo[len (nome_completo)-1]))
