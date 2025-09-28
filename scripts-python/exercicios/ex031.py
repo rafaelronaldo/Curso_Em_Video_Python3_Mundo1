44f7f45521d115a823f8316872faed629dcd1331
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-09-28 14:56:46 -0300

    Exercicio 31

---
+++ scripts-python/exercicios/ex031.py
@@ -0,0 +1,7 @@
+distancia_viagem = float(input("Qual é a distância da sua viagem? "))
+print("Você está prester a começar uma viagem de {}Km".format(distancia_viagem))
+if distancia_viagem <=200:
+    preco = distancia_viagem * 0.50
+else:
+    preco = distancia_viagem * 0.45
+print("E o preço da sua passagem será de R${:0.2f}".format(preco))
