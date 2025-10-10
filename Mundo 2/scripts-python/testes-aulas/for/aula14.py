651d5b6ad5da1627f106ccf9e657e9e5b5a69f64
Rafael Ronaldo <rafaelronaldo.dev@gmail.com>
2025-10-09 23:23:22 -0300

    Aula 14

---
+++ Mundo 2/scripts-python/testes-aulas/for/aula14.py
@@ -0,0 +1,24 @@
+'''for contador in range(1,10):
+    print(contador)
+print("Fim")'''
+
+'''c = 1
+while c < 10:
+    print(c)
+    c += 1'''
+
+'''for contador in range (1,5):
+    numero = int(input("Digite um valor: "))
+print("Fim")'''
+
+'''numero = 1
+while numero != 0:
+    numero = int(input("Digite um número: "))
+print("Fim")'''
+
+r = "S"
+while r == "S":
+    numero = int(input("Digite um número: "))
+    r = str(input("Quer continuar? [S/N]")).upper()
+print("Fim")
+
